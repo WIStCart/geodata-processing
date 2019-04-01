a4cce7a2248dfb5faaafd454fe4e6a21b9393b56
bensegal7 <ben.segal7@gmail.com>
2019-04-01 10:57:25 -0500

    Comments addressed, and more error catching for process

--- scripts/process.py
+++ scripts/process.py
@@ -3,8 +3,7 @@
 # Description: script used for metadata transformations and parsing
 # Dependencies: Python 3.x, lxml
 
-###### I updated xslt paths to reference copies that sit on Jaime's server
-###### moving forward let's use this path.  You should have an R: drive now... see slack
+# Absolute path of XSLT docs 
 isoxslt = 'R:\\SCRIPTS\\xslt\\ArcGIS2ISO19139_uw-geodata.xsl'
 jsonxslt = 'R:\\SCRIPTS\\xslt\\iso2geoBL_uw-geodata.xsl'
 
@@ -16,47 +15,33 @@ import os
 
 # Non standard python libraries
 # requires additional installation
-# lxml: py -m pip install lxml
-
-###### is py versus python as standard thing?  "py" does not work on my computer.
-###### I had to edit process.bat to get things working on my PC
+# lxml: python -m pip install lxml
 import lxml.etree as ET
 
 # Establish input directory and metadata types for data transformations
 
-##### take a second look at how you are handling the arguments
-##### when no output is provided, I would expect to see something akin to the following (example):
-
-"""
-
-usage: update.py [-h] [-r]
-                 (-aj ADD_JSON | -dc DELETE_COLLECTION | -d DELETE | -p)
-                 [-i {test,prod,dev}]
-update.py: error: one of the arguments -aj/--add-json -dc/--delete-collection -d/--delete -p/--purge is required
-
-"""
-
-###### take a look at the argument handling in update.py for some ideas.
-
-
 parser= argparse.ArgumentParser()
 parser.add_argument('-i', '--input', help='Directory of input files')
-parser.add_argument('-fi', '--input_format', help='Metadata format of input files', required=False, choices={"esri", "Esri","ESRI", "iso", "ISO"})
-parser.add_argument('-o', '--output', help='Directory for output files')
-parser.add_argument('-fo', '--output_format', help='Metadata format for output files', required=False, choices={"iso", "ISO", "gbl", "GBL"})
-########## No need to handle multiple combinations of case.. iso, ISO, Esri, ESRI, etc.  Just convert everything to lowercase after input is received.  I think the syntax is stringname.lower()????
+parser.add_argument('-fi', '--input_format', help="Metadata format of input files", required=False, choices={"esri", "iso"})
+parser.add_argument('-o', '--output', help="Directory for output files")
+parser.add_argument('-fo', '--output_format', help="Metadata format for output files", required=False, choices={"iso", "gbl"})
 args = parser.parse_args()
 inPath = args.input
 outPath = args.output
 inForm = args.input_format
 outForm = args.output_format
 
+
 # Set file types if user leaves these options blank
 if (inForm is None):
-    ###### actually this should be iso not esri
-    inForm = 'esri'
+    inForm = 'iso'
 if (outForm is None):
     outForm = 'gbl'
+
+inForm = inForm.lower()
+outForm = outForm.lower()
+
+
 
 try:
     if (inPath[-1] != '\\'):
@@ -74,7 +59,9 @@ try:
     # Transform ISO 19139 metadata to json format
     def iso2gbl (xmlList, jsonxslt, ins, out):
         gblCount = 0
+        print("\nISO 19139 to GBL: ")
         for xml in xmlList:
+            print(xml)
             if (inForm == 'esri' or inForm == 'Esri' or inForm == 'ESRI'):
                 filename = xml[:-8]
             else:
@@ -88,49 +75,43 @@ try:
             f = open(out + filename + '.json', 'w', encoding="utf-8")
             f.write(str(result))
             f.close()
-            ######## suggest writing out the filename as output is created.  This helps the user know that something is happening.
             gblCount += 1
-            ######## instead of going thru all of the follow tests, just convert the input to all lower case. This will greatly simplify some of the statements below.
-            
-            ##### also, new information: turns out we should save the _iso file that is created as part of the process.  So you can remove the following
-            
-            if (inForm == 'esri' or inForm == 'Esri' or inForm == 'ESRI'):
-                os.remove(ins + filename + '_iso.xml')
-        print (str(gblCount) + " records transformed from " + inForm + " to " + outForm)
 
     # Transform esri metadata to ISO 19139 format
     def esri2iso (xmlList, isoxslt, ins, out):
         isoCount = 0
+        print("\nEsri to ISO 19139: ")
         for xml in onlyfiles:
+            print(xml)
             filename = xml[:-4]
             dom = ET.parse(ins + xml)
             xslt = ET.parse(isoxslt)
             transform = ET.XSLT(xslt)
             newdom = transform(dom)
-            ####### same comments as above, print out an indication of what's being processed so the user knows something is happening
             outfile = open(out + filename + '_iso.xml', 'w', encoding="utf-8")
             outfile.write(str(newdom))
             outfile.close()
             isoCount += 1
-        if (outForm == 'iso' or outForm == 'ISO'):
-            print (str(isoCount) + " records transformed from " + inForm + " to " + outForm)
 
     # Run transformations based on input file types
-    
-    
-    ###### what happens if the user inputs an invalid combination of -fi and -fo? eg., -fi gbl -fo esri, -fi iso -fo esri, -fi iso -fo iso etc.  Try some combos.  Hint: more error checking needed.
-    
-    #######  see comments above about converting all input to lowercase.  
-    
-    if ((inForm == 'iso' or inForm == 'ISO') and (outForm == 'gbl' or outForm == 'GBL')):
+    if ((inForm == 'iso') and (outForm == 'gbl')):
         iso2gbl(onlyfiles, jsonxslt, inPath, outPath)
-    elif ((inForm == 'esri' or inForm == 'Esri' or inForm == 'ESRI') and (outForm == 'iso' or outForm == 'ISO')):
+    elif ((inForm == 'esri') and (outForm == 'iso')):
         esri2iso(onlyfiles, isoxslt, inPath, outPath)
-    elif ((inForm == 'esri' or inForm == 'Esri' or inForm == 'ESRI') and (outForm == 'gbl' or outForm == 'GBL')):
+    elif ((inForm == 'esri') and (outForm == 'gbl')):
         esri2iso(onlyfiles, isoxslt, inPath, outPath)
         isofiles = [f for f in listdir(outPath) if (isfile(join(outPath, f)) and 'iso.xml' in f)]
         iso2gbl(isofiles, jsonxslt, outPath, outPath)
+    else:
+        print("Invalid input/output formats. \n Valid input formats: 'esri', 'iso' \n Valid output formats: 'iso', 'gbl'")
+    print('')
         
-
+# Print message/usage if incorrect or blank paths are entered
 except TypeError:
-    print("Ensure the input and output file paths and types are entered correctly.")
+    print("\nEnsure the input and output file paths are entered correctly. \n")
+    parser.print_help()
+
+except FileNotFoundError:
+    print("\nEnsure the input and output file paths are entered correctly. \n")
+    parser.print_help()
+
