f809ef314eb58a3daf56d56f8cbd0286bf95af86
bensegal7 <ben.segal7@gmail.com>
2019-04-18 16:39:33 -0500

    esriscanner
    Calls update.py after records from a dataset are archived

--- scripts/esriscanner.py
+++ scripts/esriscanner.py
@@ -12,6 +12,14 @@ Dependencies: Python 3.x
 import json
 import urllib.request
 from html.parser import HTMLParser
+import shutil
+import os
+
+shutil.rmtree('C:/PROJECTS/Directed_Studies/esriscanner/data-ltsb')
+shutil.rmtree('C:/PROJECTS/Directed_Studies/esriscanner/data-wi-dnr')
+os.makedirs('C:/PROJECTS/Directed_Studies/esriscanner/data-ltsb')
+os.makedirs('C:/PROJECTS/Directed_Studies/esriscanner/data-wi-dnr')
+
 
 # Strip html from description
 class MLStripper(HTMLParser):
@@ -31,28 +39,36 @@ def strip_tags(html):
     return s.get_data()
 
 # Create GBL records from LTSB data
+# open and read data.json
 s = urllib.request.urlopen("https://data-ltsb.opendata.arcgis.com/data.json").read()
 d = json.loads(s.decode('utf-8'))
 with open('C:\\PROJECTS\\Directed_Studies\\esriscanner\\test.json', 'w') as fout:
     json.dump(d, fout, indent=1)
+# loop through each record
 for x in d["dataset"]:
+    # call html strip function
     description = strip_tags(x["description"]).replace('Attribute Field Definitions', '')
     access = x["accessLevel"].capitalize()
     envelope = "ENVELOPE(" + x["spatial"] + ")"
     identifier = x["identifier"]
+    # create slug from end of identifier and 'LTSB_OpenData_'
     slug = "LTSB_OpenData_" + x["identifier"].replace('http://data-ltsb.opendata.arcgis.com/datasets/','')
     mod = x["modified"]
     geomType = ""
+    # grab raw metadata if available and get geom type
     if("FeatureServer" in x["webService"]):
         a = urllib.request.urlopen(x["webService"] + "?f=pjson").read()
         b = json.loads(a.decode('utf-8'))
         geomType = b["geometryType"].replace('esriGeometry', '')
+    # create references from distribution 
     references = "{"
     for t in x["distribution"]:
         if 'downloadURL' in t:
             references += '\"http://schema.org/downloadUrl\":\"' + t["downloadURL"] + '\",'
+    references += '\"http://schema.org/url\":\"' + x["identifier"] + '\",'
     references += "}"
     references = references.replace(",}", "}")
+    # format gbl record
     temp_obj = {
         "geoblacklight_version": "1.0",
         "dc_identifier_s": identifier,
@@ -77,24 +93,33 @@ for x in d["dataset"]:
         "solr_year_i": mod[0:4], # Modified year used
         "dct_references_s": references
     }
+    # save GBL record to LTSB data directory
     outTitle = x["title"].replace(' ', '_')
     outTitle = outTitle.replace('*', '')
     with open('C:\\PROJECTS\\Directed_Studies\\esriscanner\\data-ltsb\\' + outTitle + ".json", 'w') as k:
         json.dump(temp_obj, k, indent=1)
 
+# call update script to injest LTSB GBL records
+os.system("py update.py -aj C:\\PROJECTS\\Directed_Studies\\esriscanner\\data-ltsb\\ -i test")
+
 # Create GBL records from DNR data
+# open and read data.json
 dnrRead = urllib.request.urlopen("http://data-wi-dnr.opendata.arcgis.com/data.json").read()
 dnrJson = json.loads(dnrRead.decode('utf-8'))
 with open('C:\\PROJECTS\\Directed_Studies\\esriscanner\\dnrTest.json', 'w') as dnrCont:
     json.dump(dnrJson, dnrCont, indent=1)
+# loop through each record
 for record in dnrJson["dataset"]:
+    # call html strip function
     description = strip_tags(record["description"])
     access = record["accessLevel"].capitalize()
     envelope = "ENVELOPE(" + record["spatial"] + ")"
     identifier = record["identifier"]
+    # create slug from end of identifier and 'WI_DNR_'
     slug = "WI_DNR_" + record["identifier"].replace('http://data-wi-dnr.opendata.arcgis.com/datasets/','')
     mod = record["modified"]
     geomType = ""
+    # grab raw metadata if available and get geom type
     if("FeatureServer" in record["webService"] or "MapServer" in record["webService"]):
         meta = urllib.request.urlopen(record["webService"] + "?f=pjson").read()
         metajson = json.loads(meta.decode('utf-8'))
@@ -102,12 +127,19 @@ for record in dnrJson["dataset"]:
             geomType = metajson["geometryType"].replace('esriGeometry', '')
     else:
         geomType = ""
+    # create references from distribution, webService, and identifier
     references = "{"
     for dis in record["distribution"]:
         if 'downloadURL' in dis:
             references += '\"http://schema.org/downloadUrl\":\"' + dis["downloadURL"] + '\",'
+    if ('https://p.widencdn.net/' in record["webService"]):
+        references += '\"http://schema.org/downloadUrl\":\"' + record["webService"] + '\",'
+    if (record["webService"].endswith('.zip')):
+        references += '\"http://schema.org/downloadUrl\":\"' + record["webService"] + '\",'
+    references += '\"http://schema.org/url\":\"' + record["identifier"] + '\",'
     references += "}"
     references = references.replace(",}", "}")
+    # format gbl record
     dnr_temp_obj = {
         "geoblacklight_version": "1.0",
         "dc_identifier_s": identifier,
@@ -132,8 +164,12 @@ for record in dnrJson["dataset"]:
         "solr_year_i": mod[0:4], # Modified year used
         "dct_references_s": references
     }
+    # save GBL record to LTSB data directory
     outTitle = record["title"].replace(' ', '_')
     outTitle = outTitle.replace('*', '')
     outTitle = outTitle.replace('/', '')
     with open('C:\\PROJECTS\\Directed_Studies\\esriscanner\\data-wi-dnr\\' + outTitle + ".json", 'w') as k:
         json.dump(dnr_temp_obj, k, indent=1)
+
+# call update script to injest DNR GBL records
+os.system("py update.py -aj C:\\PROJECTS\\Directed_Studies\\esriscanner\\data-wi-dnr\\ -i test")
