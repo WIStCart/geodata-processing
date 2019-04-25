0dc68aec2683b4b05aa37adbd38e3b9f7c52b159
bensegal7 <ben.segal7@gmail.com>
2019-04-25 12:43:29 -0500

    QA check added

--- scripts/esriscanner.py
+++ scripts/esriscanner.py
@@ -15,10 +15,10 @@ from html.parser import HTMLParser
 import shutil
 import os
 
-shutil.rmtree('C:/PROJECTS/Directed_Studies/esriscanner/data-ltsb')
-shutil.rmtree('C:/PROJECTS/Directed_Studies/esriscanner/data-wi-dnr')
-os.makedirs('C:/PROJECTS/Directed_Studies/esriscanner/data-ltsb')
-os.makedirs('C:/PROJECTS/Directed_Studies/esriscanner/data-wi-dnr')
+# Non standard python libraries
+# requires additional installation
+# ruamel.yaml: python -m pip install ruamel.yaml
+import ruamel.yaml as yaml
 
 
 # Strip html from description
@@ -38,138 +38,98 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
-# Create GBL records from LTSB data
-# open and read data.json
-s = urllib.request.urlopen("https://data-ltsb.opendata.arcgis.com/data.json").read()
-d = json.loads(s.decode('utf-8'))
-with open('C:\\PROJECTS\\Directed_Studies\\esriscanner\\test.json', 'w') as fout:
-    json.dump(d, fout, indent=1)
-# loop through each record
-for x in d["dataset"]:
-    # call html strip function
-    description = strip_tags(x["description"]).replace('Attribute Field Definitions', '')
-    access = x["accessLevel"].capitalize()
-    envelope = "ENVELOPE(" + x["spatial"] + ")"
-    identifier = x["identifier"]
-    # create slug from end of identifier and 'LTSB_OpenData_'
-    slug = "LTSB_OpenData_" + x["identifier"].replace('http://data-ltsb.opendata.arcgis.com/datasets/','')
-    mod = x["modified"]
-    geomType = ""
-    # grab raw metadata if available and get geom type
-    if("FeatureServer" in x["webService"]):
-        a = urllib.request.urlopen(x["webService"] + "?f=pjson").read()
-        b = json.loads(a.decode('utf-8'))
-        geomType = b["geometryType"].replace('esriGeometry', '')
-    # create references from distribution 
-    references = "{"
-    for t in x["distribution"]:
-        if 'downloadURL' in t:
-            references += '\"http://schema.org/downloadUrl\":\"' + t["downloadURL"] + '\",'
-    references += '\"http://schema.org/url\":\"' + x["identifier"] + '\",'
-    references += "}"
-    references = references.replace(",}", "}")
-    # format gbl record
-    temp_obj = {
-        "geoblacklight_version": "1.0",
-        "dc_identifier_s": identifier,
-        "dc_title_s": x["title"],
-        "dc_description_s": description,
-        "dc_rights_s": access,
-        "dct_provenance_s": x["publisher"]["name"],
-        "layer_id_s": "", # leave blank? 
-        "layer_slug_s": slug,
-        "layer_geom_type_s": geomType, # type not in data.json
-        "layer_modified_dt": x["modified"],
-        "dc_format_s": "Shapefile", # all esri sites so probably shapefile?
-        "dc_language_s": "English",
-        "dct_isPartOf_sm": "Wisconsin Legislative Technology Services Bureau Open Data",
-        "dc_creator_sm": x["publisher"]["name"],
-        "dc_type_s": "Dataset",
-        "dc_subject_sm": x["keyword"],
-        "dct_spatial_sm": "", # no spatial field in data.json
-        "dct_issued_s": x["issued"],
-        "dct_temporal_sm": "", # which date?
-        "solr_geom": envelope,
-        "solr_year_i": mod[0:4], # Modified year used
-        "dct_references_s": references
-    }
-    # save GBL record to LTSB data directory
-    outTitle = x["title"].replace(' ', '_')
-    outTitle = outTitle.replace('*', '')
-    with open('C:\\PROJECTS\\Directed_Studies\\esriscanner\\data-ltsb\\' + outTitle + ".json", 'w') as k:
-        json.dump(temp_obj, k, indent=1)
-
-# call update script to injest LTSB GBL records
-os.system("py update.py -aj C:\\PROJECTS\\Directed_Studies\\esriscanner\\data-ltsb\\ -i test")
-
-# Create GBL records from DNR data
-# open and read data.json
-dnrRead = urllib.request.urlopen("http://data-wi-dnr.opendata.arcgis.com/data.json").read()
-dnrJson = json.loads(dnrRead.decode('utf-8'))
-with open('C:\\PROJECTS\\Directed_Studies\\esriscanner\\dnrTest.json', 'w') as dnrCont:
-    json.dump(dnrJson, dnrCont, indent=1)
-# loop through each record
-for record in dnrJson["dataset"]:
-    # call html strip function
-    description = strip_tags(record["description"])
-    access = record["accessLevel"].capitalize()
-    envelope = "ENVELOPE(" + record["spatial"] + ")"
-    identifier = record["identifier"]
-    # create slug from end of identifier and 'WI_DNR_'
-    slug = "WI_DNR_" + record["identifier"].replace('http://data-wi-dnr.opendata.arcgis.com/datasets/','')
-    mod = record["modified"]
-    geomType = ""
-    # grab raw metadata if available and get geom type
-    if("FeatureServer" in record["webService"] or "MapServer" in record["webService"]):
-        meta = urllib.request.urlopen(record["webService"] + "?f=pjson").read()
-        metajson = json.loads(meta.decode('utf-8'))
-        if ("geometryType" in metajson):
-            geomType = metajson["geometryType"].replace('esriGeometry', '')
-    else:
+def json2gbl (jsonUrl, collection, siteName, partOf):
+    if (os.path.isdir('C:/PROJECTS/Directed_Studies/esriscanner/' + collection) == True):
+        shutil.rmtree('C:/PROJECTS/Directed_Studies/esriscanner/' + collection)
+    os.makedirs('C:/PROJECTS/Directed_Studies/esriscanner/' + collection)
+    s = urllib.request.urlopen(jsonUrl).read()
+    d = json.loads(s.decode('utf-8'))
+    for x in d["dataset"]:
+        # call html strip function
+        description = strip_tags(x["description"])
+        if (collection == "LTSB_OpenData"):
+            description = description.replace('Attribute Field Definitions', '')
+        access = x["accessLevel"].capitalize()
+        innerEnv = [l for l in x["spatial"].split(',')]
+        envelope = "ENVELOPE(" + innerEnv[0] + ", " +  innerEnv[2] + ", " + innerEnv[3] + ", " + innerEnv[1] + ")"
+        identifier = x["identifier"]
+        # create slug from end of identifier and 'LTSB_OpenData_'
+        slug = collection.replace("OpenData", "") + "_" + x["identifier"].replace('http://' + siteName + '.opendata.arcgis.com/datasets/','')
+        mod = x["modified"]
         geomType = ""
-    # create references from distribution, webService, and identifier
-    references = "{"
-    for dis in record["distribution"]:
-        if 'downloadURL' in dis:
-            references += '\"http://schema.org/downloadUrl\":\"' + dis["downloadURL"] + '\",'
-    if ('https://p.widencdn.net/' in record["webService"]):
-        references += '\"http://schema.org/downloadUrl\":\"' + record["webService"] + '\",'
-    if (record["webService"].endswith('.zip')):
-        references += '\"http://schema.org/downloadUrl\":\"' + record["webService"] + '\",'
-    references += '\"http://schema.org/url\":\"' + record["identifier"] + '\",'
-    references += "}"
-    references = references.replace(",}", "}")
-    # format gbl record
-    dnr_temp_obj = {
-        "geoblacklight_version": "1.0",
-        "dc_identifier_s": identifier,
-        "dc_title_s": record["title"],
-        "dc_description_s": description,
-        "dc_rights_s": access,
-        "dct_provenance_s": record["publisher"]["name"],
-        "layer_id_s": "", # leave blank? 
-        "layer_slug_s": slug,
-        "layer_geom_type_s": geomType, # type not in data.json
-        "layer_modified_dt": record["modified"],
-        "dc_format_s": "Shapefile", # all esri sites so probably shapefile?
-        "dc_language_s": "English",
-        "dct_isPartOf_sm": "Wisconsin Legislative Technology Services Bureau Open Data",
-        "dc_creator_sm": record["publisher"]["name"],
-        "dc_type_s": "Dataset",
-        "dc_subject_sm": record["keyword"],
-        "dct_spatial_sm": "", # no spatial field in data.json
-        "dct_issued_s": record["issued"],
-        "dct_temporal_sm": "", # which date?
-        "solr_geom": envelope,
-        "solr_year_i": mod[0:4], # Modified year used
-        "dct_references_s": references
-    }
-    # save GBL record to LTSB data directory
-    outTitle = record["title"].replace(' ', '_')
-    outTitle = outTitle.replace('*', '')
-    outTitle = outTitle.replace('/', '')
-    with open('C:\\PROJECTS\\Directed_Studies\\esriscanner\\data-wi-dnr\\' + outTitle + ".json", 'w') as k:
-        json.dump(dnr_temp_obj, k, indent=1)
+        # grab raw metadata if available and get geom type
+        if("FeatureServer" in x["webService"] or "MapServer" in x["webService"]):
+            a = urllib.request.urlopen(x["webService"] + "?f=pjson").read()
+            b = json.loads(a.decode('utf-8'))
+            if("geometryType" in b):
+                geomType = b["geometryType"].replace('esriGeometry', '')
+        # create references from distribution 
+        references = "{"
+        for t in x["distribution"]:
+            if 'downloadURL' in t:
+                references += '\"http://schema.org/downloadUrl\":\"' + t["downloadURL"] + '\",'
+        if ('https://p.widencdn.net/' in x["webService"]):
+            references += '\"http://schema.org/downloadUrl\":\"' + x["webService"] + '\",'
+        if (x["webService"].endswith('.zip')):
+            references += '\"http://schema.org/downloadUrl\":\"' + x["webService"] + '\",'
+        references += '\"http://schema.org/url\":\"' + x["identifier"] + '\",'
+        references += "}"
+        references = references.replace(",}", "}")
+        # format gbl record
+        temp_obj = {
+            "geoblacklight_version": "1.0",
+            "dc_identifier_s": identifier,
+            "dc_title_s": x["title"],
+            "dc_description_s": description,
+            "dc_rights_s": access,
+            "dct_provenance_s": x["publisher"]["name"],
+            "layer_id_s": "", 
+            "layer_slug_s": slug,
+            "layer_geom_type_s": geomType, 
+            "layer_modified_dt": x["modified"],
+            "dc_format_s": "File", 
+            "dc_language_s": "English",
+            "dct_isPartOf_sm": partOf,
+            "dc_creator_sm": x["publisher"]["name"],
+            "dc_type_s": "Dataset",
+            "dc_subject_sm": x["keyword"],
+            "dct_spatial_sm": "", # no spatial field in data.json
+            "dct_issued_s": x["issued"],
+            "dct_temporal_sm": "", 
+            "solr_geom": envelope,
+            "solr_year_i": mod[0:4], 
+            "dct_references_s": references
+        }
+        # Check if required elements have valid data before calling update.py
+        scanCatch = "\n"
+        if(temp_obj["dc_identifier_s"] == ""):
+            scanCatch += "dc_identifier_s\n"
+        if(temp_obj["layer_slug_s"] == ""):
+            scanCatch += "layer_slug_s\n"
+        if(temp_obj["dc_title_s"] == ""):
+            scanCatch += "dc_title_s\n"
+        if("NaN" in temp_obj["solr_geom"] ):
+            scanCatch += "solr_geom\n"
+        if(temp_obj["dct_provenance_s"] == ""):
+            scanCatch += "layer_slug_s\n"
+        if(temp_obj["dc_rights_s"] == ""):
+            scanCatch += "layer_slug_s\n"
+        if(temp_obj["geoblacklight_version"] == ""):
+            scanCatch += "geoblacklight_version\n"
+        if(temp_obj["solr_year_i"] == 9999):
+            scanCatch += "solr_year_i\n"
+        # save GBL record to collection data directory
+        outTitle = x["title"].replace(' ', '_')
+        outTitle = outTitle.replace('*', '')
+        outTitle = outTitle.replace('/', '')
+        if (scanCatch == "\n"):
+            with open('C:\\PROJECTS\\Directed_Studies\\esriscanner\\' + collection + "\\" + outTitle + ".json", 'w') as k:
+                json.dump(temp_obj, k, indent=1)
+    os.system("python update.py -aj C:\\PROJECTS\\Directed_Studies\\esriscanner\\" + collection + "\\ -i prod")
 
-# call update script to injest DNR GBL records
-os.system("py update.py -aj C:\\PROJECTS\\Directed_Studies\\esriscanner\\data-wi-dnr\\ -i test")
+# loop through each collection in OpenData.yml and call json2gbl function
+with open("C:\\PROJECTS\\Directed_Studies\\esriscanner\\OpenData.yml") as stream:
+    theDict = yaml.safe_load(stream)
+    for record in theDict["Sites"]:
+        siteRecord = theDict["Sites"][record]
+        json2gbl(siteRecord["SiteURL"], record , siteRecord["SiteShort"], siteRecord["CollectionName"])
