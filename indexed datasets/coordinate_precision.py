ee487f3c55529a03cc95954ba30712354709b392
Hayden Elza <HaydenElza@users.noreply.github.com>
2022-04-21 13:53:31 -0500

    Consolidated code into indextools.py
    Consolidated code from `checkurls.py` and `coordinate_precision.py` into `indextools.py`. The two scripts shared a fair bit of code and it made sense to combine them to share common functions.

--- indexed datasets/coordinate_precision.py
+++ indexed datasets/coordinate_precision.py
@@ -4,14 +4,12 @@
 # Read indexed dataset GeoJSON and reduce coordinate precision.
 # Hayden Elza (hayden.elza@gmail.com)
 # Created: 2022-03-22
-# Modified: 
+# Modified: 2022-04-21
 #------------------------------------------------------------------------------
 
 
-import os
-import json
-import logging, datetime
 import argparse
+from indextools import coordinate_precicison
 
 
 def parse_arguments():
@@ -29,102 +27,12 @@ def parse_arguments():
     parser.add_argument("-v", "--verbose", help="Write successful precision changes to log as well.", dest='verbose', action='store_true')
 
     # Print version
-    parser.add_argument("--version", action="version", version="%(prog)s - Version 1.0")
+    parser.add_argument("--version", action="version", version="%(prog)s - Version 1.1")
 
     # Parse arguments
     args = parser.parse_args()
 
     return args
-
-
-def reduce_precision(dataset, out_path, precision, indentation, skip_feature, verbose):
-
-    # Open geojson
-        with open(dataset, 'r') as f:
-            data = json.load(f)
-
-        # Lower precision of tile coordinates
-        for feature in data['features']:
-
-            # Polygon
-            if feature['geometry']['type'] == 'Polygon':
-                feature['geometry']['coordinates'][0] = [[round(coord, precision) for coord in coords] for coords in feature['geometry']['coordinates'][0]]
-
-            # MultiPolygon
-            elif feature['geometry']['type'] == 'MultiPolygon':
-                feature['geometry']['coordinates'][0][0] = [[round(coord, precision) for coord in coords] for coords in feature['geometry']['coordinates'][0][0]]
-
-            # Point
-            elif feature['geometry']['type'] == 'Point':
-                feature['geometry']['coordinates'] = [round(coord, precision) for coord in feature['geometry']['coordinates']]
-            
-            # Other
-            else:
-
-                # If flag to skip only feature is used
-                if skip_feature:
-                    logging.warning("{} feature {}: Feature type {} is not supported. Skipping!".format(os.path.basename(dataset), data['features'].index(feature), feature['geometry']['type']))
-
-                # Else skip entire dataset
-                else:
-                    logging.warning("{} feature {}: Feature type {} is not supported. Skipping Dataset!".format(os.path.basename(dataset), data['features'].index(feature), feature['geometry']['type']))
-                    return
-
-         # Log dataset if verbose flag used
-        if verbose:
-            logging.info("Updated precision of {} to {}.".format(os.path.basename(dataset), precision))
-
-        # Write updated json to file
-        with open(os.path.join(out_path, os.path.basename(dataset)), 'w') as f:
-            json.dump(data, f, indent=indentation)
-
-        return
-
-
-def coordinate_precicison(in_path, out_path, precision, indentation, skip_feature, verbose):
-
-    # Start log
-    logging.basicConfig(filename='coordinate_precision.log', level=logging.INFO, filemode='w')
-    start = datetime.datetime.now()
-    logging.info("Started: {}".format(start.strftime('%Y-%m-%d %H:%M:%S')))
-
-    # Datasets to check
-    datasets = []
-
-    # If input path is single file
-    if os.path.isfile and os.path.splitext(in_path)[1] == ".geojson":
-        datasets.append(in_path)
-
-    # If input path is directory
-    else: 
-        for filename in os.listdir(in_path):
-        
-            # Get file name and extension
-            f = os.path.join(in_path, filename)
-            extension = os.path.splitext(filename)[1]
-
-            # If geojson, add dataset to list
-            if extension == ".geojson":
-                datasets.append(f)
-
-    # Make sure output path exists
-    if not os.path.exists(out_path):
-
-        # If it does not yet exist, make it
-        os.mkdir(out_path)
-        logging.info("Making {}.".format(out_path))
-
-
-    for dataset in datasets:
-        reduce_precision(dataset, out_path, precision, indentation, skip_feature, verbose)
-    
-    # End log
-    end = datetime.datetime.now()
-    logging.info("Ended: {}".format(end.strftime('%Y-%m-%d %H:%M:%S')))
-    elapsed_time = end - start
-    m, s = divmod(elapsed_time.total_seconds(), 60)
-    h, m = divmod(m, 60)
-    logging.info("Time to complete: {} hours, {} minutes, {} seconds".format(h, m, s))
 
 if __name__ == '__main__':
     # Parse arguments
