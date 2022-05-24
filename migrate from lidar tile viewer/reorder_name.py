1410f4487cd3b088814a6e8981842029197832d8
KCivilEnGI <101383171+KCivilEnGI@users.noreply.github.com>
2022-05-24 11:56:50 -0500

    Add reorder_name.py
    Add reorder_name.py with pseudo-code.

---
+++ migrate from lidar tile viewer/reorder_name.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# reorder_name.py
+# Reorder pieces of filenames in GeoJSONs
+# Kuang-Cheng Cheng (kcheng38@wisc.edu)
+# Created: 2022-05-24
+# Modified: 
+#------------------------------------------------------------------------------
+
+# Import libraries
+
+# Open geojson
+
+# Iterrate through features
+
+    # Read the download URL
+
+    # Get the filename
+
+    # Break the filename into pieces (township/range/section)
+
+    # Concatenate a new filename (Use zfill)
+
+    # Store new filename into download URL
+
+# Write the GeoJSON to a new file (indent=2)
+
